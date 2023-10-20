809db3cb3df1cd8b39fef0057da7cc6f6674c6e3
murugan001ab <105478973+murugan001ab@users.noreply.github.com>
2023-10-20 16:44:21 +0530

    Update pattern.py
    hacktoberfest,Hacktoberfest2023

--- pattern.py
+++ pattern.py
@@ -1,3 +1,5 @@
 number=int(input("enter the number:"))
-for i in range(1,number+1):
-    print(i*"*")
+for i in range(1,number):
+    for j in range(1,i+1):
+        print("*")
+    print(" ",end=" ")
