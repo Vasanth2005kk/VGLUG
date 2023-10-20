3a2c567237ed7233fb05f4dd4fcd08972b0a3ecb
Vasanth2005kk <139038225+Vasanth2005kk@users.noreply.github.com>
2023-10-20 12:42:51 +0530

    Delete 0_try_example.py

--- 0_try_example.py
+++
@@ -1,6 +0,0 @@
-def count_substring(string, sub_string):
-    for i in range(string):
-        return
-string=input("enter the string:")
-sub_string=input("enter the string:")
-count_substring(string,sub_string)
