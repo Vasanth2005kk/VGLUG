6bf363f84b9712ab6069d509c3d660734c7f65db
Vasanth2005kk <vasanth2005kk@gmail.com>
2024-03-21 06:01:54 +0530

    substring_and_unic.py

---
+++ substring_and_unic.py
@@ -0,0 +1,17 @@
+try:
+    string=input('enter the string:') 
+
+    output=[]
+    for i in range(len(string)):
+        for j in range(i+2,len(string)+1):
+            output.append(string[i:j])
+
+    for k in output:
+        if len(set(list(k)))==len(list(k)):
+            print(k)
+        else:
+            print("NOt unic elements !")
+
+except Exception as e:
+    print("ERROR:",e)
+
