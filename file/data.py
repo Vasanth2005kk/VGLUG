ccaecf95b4274944501b7b72e2886e6dfae9f0b4
Bharathi V <128215224+Bharathiv1403@users.noreply.github.com>
2024-10-07 15:57:33 +0530

    Add the code in python programs

--- file/data.py
+++ file/data.py
@@ -10,3 +10,57 @@ output=" ".join(emt_list)
 print(output)
 file_1.write(output)
 file_1.close()
+
+
+import os
+
+def create_file(filename):
+    try:
+        with open(filename, 'w') as f:
+            f.write('Hello, world!\n')
+        print("File " + filename + " created successfully.")
+    except IOError:
+        print("Error: could not create file " + filename)
+
+def read_file(filename):
+    try:
+        with open(filename, 'r') as f:
+            contents = f.read()
+            print(contents)
+    except IOError:
+        print("Error: could not read file " + filename)
+
+def append_file(filename, text):
+    try:
+        with open(filename, 'a') as f:
+            f.write(text)
+        print("Text appended to file " + filename + " successfully.")
+    except IOError:
+        print("Error: could not append to file " + filename)
+
+def rename_file(filename, new_filename):
+    try:
+        os.rename(filename, new_filename)
+        print("File " + filename + " renamed to " + new_filename + " successfully.")
+    except IOError:
+        print("Error: could not rename file " + filename)
+
+def delete_file(filename):
+    try:
+        os.remove(filename)
+        print("File " + filename + " deleted successfully.")
+    except IOError:
+        print("Error: could not delete file " + filename)
+
+
+if __name__ == '__main__':
+    filename = "bharathi.txt"
+    new_filename = "new_example.txt"
+
+    create_file(filename)
+    read_file(filename)
+    append_file(filename, "This is some additional text.\n")
+    read_file(filename)
+    rename_file(filename, new_filename)
+    read_file(new_filename)
+    delete_file(new_filename)
